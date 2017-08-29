caa86f8d79a80e193eadeaf2da786ebb4821203a
Saavan Dhaliwal <16569@cgs.act.edu.au>
2017-08-29 12:39:58 +1000

    Initial Commit

---
+++ Foodiary/Foodiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foodiary
+//
+//  Created by Saavan Dhaliwal on 29/8/17.
+//  Copyright © 2017 Saavan Dhaliwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
